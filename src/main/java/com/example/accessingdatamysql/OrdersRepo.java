df90e205c1bd35dc4c4af6a6119e8decb7da2054
RobinBroberg <robin.broberg86@gmail.com>
2023-05-22 11:21:29 +0200

    added properties

--- src/main/java/com/example/accessingdatamysql/OrdersRepo.java
+++ src/main/java/com/example/accessingdatamysql/OrdersRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrdersRepo extends JpaRepository<Orders, Long> {
-    List<Orders> findOrdersByCustomer_Id(Long customerId);
+
 
 }
